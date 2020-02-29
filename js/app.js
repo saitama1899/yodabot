1fdfa8d49cb8f4ea8fcb80adf0178153a831a67a
saitama1899 <eric.lorien92@gmail.com>
2020-02-29 20:19:41 +0100

    Focus on input after recieving a message

--- js/app.js
+++ js/app.js
@@ -17,8 +17,8 @@ const chat_form = {
       <p v-show="writing" class="feedback" ><span class="spinner-border text-success" style="width: 1rem; height: 1rem;" role="status"></span><span> YodaBot is writing...</span></p>
       <form class="form-inline" id="chat-form" autocomplete="off" @submit.prevent="send">
         <div class="form-group">
-          <input type="text" name="message" v-model="message" class="form-control" placeholder="Say something to YodaBot"
-            required required-pattern="[A-Za-z0-9]{2,50}" :disabled="writing" autofocus>
+          <input type="text" name="message" id="input-message" v-model="message" class="form-control" placeholder="Say something to YodaBot"
+            required required-pattern="[A-Za-z0-9]{2,50}" autofocus>
           <button type="submit" class="btn btn-primary ml-2" :disabled="writing" >Send!</button>
         </div>
       </form>
@@ -46,7 +46,7 @@ const chat_form = {
           })
           .catch(e => console.log(e))
           .finally(() =>
-            this.writing = false
+            this.writing = false,
           )
         this.message = ""
       }
@@ -104,6 +104,7 @@ const chat = {
       setTimeout(function() {
       	window.scrollTo(0,document.body.scrollHeight)
       }, 10)
+      document.getElementById("input-message").focus()
   	}
   }
 }
